c570faa2b620db389429d2b2e452f3d823037f94
guilherme de brida <guilhermedbdb@gmail.com>
2022-06-22 20:43:26 -0300

    Problema De cors Resolvido

--- src/servidor.js
+++ src/servidor.js
@@ -3,6 +3,14 @@ const express = require('express')
 const app = express()
 const routes = require('./api/routes')
 const PORT = process.env.PORT
+const cors = require('cors')
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+  };  
+app.use(cors(corsOptions));
+
 app.use('/api', routes)
 
 app.use(function(err, req, res, next){
@@ -13,4 +21,4 @@ app.use(function(err, req, res, next){
 
 app.listen(PORT,function(){
     console.log("lendo",PORT)
-})    
+})
